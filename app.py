749ab59046f337b22f664e15f4935c9ee66499e8
Joshi Dev Jitenkumar <devjjoshi0910@gmail.com>
2024-03-20 22:32:16 +0530

    undo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 app = Flask(__name__)
 CORS(app)
 
-captions_string=""
+
 @app.route('/process_video', methods=['POST'])
 def process_video():
     try:
@@ -48,67 +48,13 @@ def process_video():
 
         # if captions_string:
         #     print(f"Captions for {youtube_url}:\n{captions_string}")
-        
-        # Choose pre-trained model
-        model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 
-        givinput="Earth has a very hospitable temperature and mix of chemicals that have made life abundant here. Most notably, Earth is unique in that most of our planet is covered in liquid water, since the temperature allows liquid water to exist for extended periods of time. Earth's vast oceans provided a convenient place for life to begin about 3.8 billion years ago."
-        # User input
-        paragraph = captions_string
-        question = "Which phone we are talking about ?"
-
-        # Tokenize inputs
-        inputs = tokenizer(question, paragraph, return_tensors="pt")
-
-        # Generate response
-        outputs = model(**inputs)
-        start_scores = outputs.start_logits
-        end_scores = outputs.end_logits
-
-        # Post-process output
-        start_index = torch.argmax(start_scores)
-        end_index = torch.argmax(end_scores)
-        answer_tokens = inputs["input_ids"][0][start_index:end_index+1]
-        answer = tokenizer.decode(answer_tokens)
-
-        # Display response
-        response_data = {'message': f'Generated Answer :{answer}'}
+        response_data = {'message': f'SuccessFull !. Now you may proceed to ask Questions !!'}
         return jsonify(response_data)
+
     except Exception as e:
         return jsonify({'error': str(e)})
 
-# @app.route('/process_output', methods=['POST'])
-# def process_output():
-#     try:      
-#         # Choose pre-trained model
-#         model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
-#         tokenizer = AutoTokenizer.from_pretrained(model_name)
-#         model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+@app.route('/process_output', methods=['POST'])
 
-#         givinput="Earth has a very hospitable temperature and mix of chemicals that have made life abundant here. Most notably, Earth is unique in that most of our planet is covered in liquid water, since the temperature allows liquid water to exist for extended periods of time. Earth's vast oceans provided a convenient place for life to begin about 3.8 billion years ago."
-#         # User input
-#         paragraph = captions_string
-#         question = "Which phone we are talking about ?"
 
-#         # Tokenize inputs
-#         inputs = tokenizer(question, paragraph, return_tensors="pt")
-
-#         # Generate response
-#         outputs = model(**inputs)
-#         start_scores = outputs.start_logits
-#         end_scores = outputs.end_logits
-
-#         # Post-process output
-#         start_index = torch.argmax(start_scores)
-#         end_index = torch.argmax(end_scores)
-#         answer_tokens = inputs["input_ids"][0][start_index:end_index+1]
-#         answer = tokenizer.decode(answer_tokens)
-
-#         # Display response
-#         response_data = {'message': f'Generated Answer :{answer}'}
-#         return jsonify(response_data)
-
-#     except Exception as e:
-#         return jsonify({'error': str(e)})
