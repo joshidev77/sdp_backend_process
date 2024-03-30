b96ae91eae5956adfc9315a7a70946a9dbafed66
Joshi Dev Jitenkumar <devjjoshi0910@gmail.com>
2024-03-30 11:16:20 +0530

    Changes

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 from pytube import YouTube
 
+global captions_string
+captions_string = ""
 app = Flask(__name__)
 CORS(app)
+
+
 @app.route('/process_video', methods=['POST'])
 def process_video():
+    global captions_string  # Declare the variable as global
+    
     try:
         # Access video ID from the request
         data = request.json
@@ -29,7 +36,7 @@ def process_video():
 
                 # Concatenate captions into a single string
                 for caption in captions:
-                    captions_text += f"{caption['text']}\n"
+                    captions_text += f"{caption['text']} "
 
                 return captions_text.strip()
 
@@ -38,14 +45,41 @@ def process_video():
                 return None
 
         youtube_url = video_id
-        # Example usage
+        # Example usage for captions
         captions_string = get_youtube_captions(youtube_url)
 
-        # if captions_string:
-        #     print(f"Captions for {youtube_url}:\n{captions_string}")
-
-        response_data = {'message': f'These are the captions for the youtube video:{captions_string}'}
+        response_data = {'message': f'Captions retrieved successfully : {captions_string}'}
         return jsonify(response_data)
 
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+
+@app.route('/process_output', methods=['POST'])
+def process_output():
+    try:
+        API_URL = "https://api-inference.huggingface.co/models/google-bert/bert-large-uncased-whole-word-masking-finetuned-squad"
+        headers = {"Authorization": "Bearer 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"}
+        data = request.json
+        question = data.get('question')
+        
+        if question is None:
+            raise ValueError('Question is not mentioned in the request.')
+        # Get predictions
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+            
+        output = query({
+	        "inputs": {
+		        "question": question,
+		        "context": captions_string
+	        },
+        })    
+            
+        answer=output['answer']
+        response_answer = {'answer': f'{answer}'}
+        return jsonify(response_answer)
+    
     except Exception as e:
         return jsonify({'error': str(e)})
