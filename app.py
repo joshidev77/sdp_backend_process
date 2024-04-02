405afcea66d13cb99fd65dbc14735ffc2fe7975b
Joshi Dev Jitenkumar <devjjoshi0910@gmail.com>
2024-04-02 13:22:55 +0530

    Final changes

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask_cors import CORS
 import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 from pytube import YouTube
+import google.generativeai as genai
 
 global captions_string
 captions_string = ""
+global finalQuestion
+finalQuestion=""
 app = Flask(__name__)
 CORS(app)
 
@@ -57,28 +60,50 @@ def process_video():
 
 @app.route('/process_output', methods=['POST'])
 def process_output():
+    global finalQuestion
     try:
-        API_URL = "https://api-inference.huggingface.co/models/google-bert/bert-large-uncased-whole-word-masking-finetuned-squad"
-        headers = {"Authorization": "Bearer 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"}
         data = request.json
         question = data.get('question')
-        
         if question is None:
             raise ValueError('Question is not mentioned in the request.')
-        # Get predictions
-        def query(payload):
-            response = requests.post(API_URL, headers=headers, json=payload)
-            return response.json()
-            
-        output = query({
-	        "inputs": {
-		        "question": question,
-		        "context": captions_string
-	        },
-        })    
-            
-        answer=output['answer']
-        response_answer = {'answer': f'{answer}'}
+        finalQuestion =captions_string +".........."+ question
+        genai.configure(api_key="AIzaSyDu3noOAGc6qsOZPUQwI4BqLqshOeM8Nsc")
+        # Set up the model
+        generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+        }
+
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
+
+        model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                        generation_config=generation_config,
+                                        safety_settings=safety_settings)
+
+        convo = model.start_chat(history=[])
+
+        convo.send_message(finalQuestion)
+        print(convo.last.text)
+        response_answer = {'answer': f'{convo.last.text}'}
         return jsonify(response_answer)
     
     except Exception as e:
